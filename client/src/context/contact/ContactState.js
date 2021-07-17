6bd30461057509ba5607338b1e8196eb0db16606
valwebd <77312053+valwebd@users.noreply.github.com>
2021-07-17 10:04:48 -0700

    dummy UI

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,124 @@
+import React, { useReducer } from 'react';
+import { v4 as uuid } from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from '../types';
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'suka zhopa',
+        email: 'asdfg@dvf.com',
+        phone: '122-123-123',
+        type: 'personal',
+      },
+      {
+        id: 2,
+        name: 'yoba boba',
+        email: 'adsf@wfd.com',
+        phone: '214-123-234',
+        type: 'personal',
+      },
+      {
+        id: 3,
+        name: 'nahui nado',
+        email: 'fss@dadad.com',
+        phone: '442-333-111',
+        type: 'professional',
+      },
+    ],
+    current: null,
+    filtered: null,
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Add Contact
+  const addContact = (contact) => {
+    contact.id = uuid.v4();
+    dispatch({
+      type: ADD_CONTACT,
+      payload: contact,
+    });
+  };
+
+  // Delete Contact
+  const deleteContact = (id) => {
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id,
+    });
+  };
+
+  // Set Current Contact
+  const setCurrent = (contact) => {
+    dispatch({
+      type: SET_CURRENT,
+      payload: contact,
+    });
+  };
+
+  // Clear Current Contact
+  const clearCurrent = () => {
+    dispatch({
+      type: CLEAR_CURRENT,
+    });
+  };
+
+  // Update Contact
+
+  const updateContact = (contact) => {
+    dispatch({
+      type: UPDATE_CONTACT,
+      payload: contact,
+    });
+  };
+
+  // Filter Contacts
+
+  const clearFilter = () => {
+    dispatch({
+      type: CLEAR_FILTER,
+    });
+  };
+
+  const filterContacts = (text) => {
+    dispatch({
+      type: FILTER_CONTACTS,
+      payload: text,
+    });
+  };
+
+  // Clear Filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        current: state.current,
+        filtered: state.filtered,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+        filterContacts,
+        clearFilter,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
